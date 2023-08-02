e165c8fc008dd164f7f2d1f81ac81d40de28136a
princeAkim226 <103947397+princeAkim226@users.noreply.github.com>
2023-08-02 19:44:33 +0000

    Update settings.py

--- inventory_api/settings.py
+++ inventory_api/settings.py
@@ -104,16 +104,6 @@ WSGI_APPLICATION = 'inventory_api.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
-}
 
 
 
