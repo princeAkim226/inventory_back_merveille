cc3e6a7b60340a6ceb57cc8b3c4273083a2bd3ba
princeAkim226 <103947397+princeAkim226@users.noreply.github.com>
2023-08-09 12:51:17 +0000

    Update custom_methods.py

--- inventory_api/custom_methods.py
+++ inventory_api/custom_methods.py
@@ -27,7 +27,7 @@ def custom_exception_handler(exc, context):
     
     response = exception_handler(exc, context)
 
-    if response is not None:
+    if response is None:
         return response
 
     exc_list = str(exc).split("DETAIL: ")
